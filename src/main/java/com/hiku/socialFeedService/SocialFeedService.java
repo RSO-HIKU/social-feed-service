023f6e32364eda2f2701e83a45fb96b19eece1a6
akcabra <andraz.klobucar@gmail.com>
2025-12-21 15:42:23 +0100

    HIKU-15: Dodani osnovni java classi

---
+++ src/main/java/com/hiku/socialFeedService/SocialFeedService.java
@@ -0,0 +1,8 @@
+package com.hiku.socialFeedService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/feed")
+public class SocialFeedService extends Application {
+}
