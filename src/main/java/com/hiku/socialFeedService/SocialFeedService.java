963ffe7ce5dfdce2312d2be3f9d668b90c93f4c9
akcabra <andraz.klobucar@gmail.com>
2026-01-03 20:56:03 +0100

    HIKU-31: Spremembe za CICD in dodan Keycloak

--- src/main/java/com/hiku/socialFeedService/SocialFeedService.java
+++ src/main/java/com/hiku/socialFeedService/SocialFeedService.java
@@ -2,7 +2,11 @@ package com.hiku.socialFeedService;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.auth.LoginConfig;
+import javax.annotation.security.DeclareRoles;
 
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
 @ApplicationPath("/api/feed")
 public class SocialFeedService extends Application {
         public SocialFeedService() {
